8ba91e693b0bb4210e0db018d2d0c0f818c708a3
Ben Ringia <benringia@gmail.com>
2020-09-19 22:29:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -83,25 +83,25 @@ scrollLinks.forEach((link) => {
 
 
 // pre-loader / video
-const btn = document.querySelector(".switch-btn");
-const video = document.querySelector(".video-container");
+// const btn = document.querySelector(".switch-btn");
+// const video = document.querySelector(".video-container");
 
-btn.addEventListener("click", function () {
-  if (!btn.classList.contains("slide1")) {
-    btn.classList.add("slide1");
-    video.pause();
-  } else {
-    btn.classList.remove("slide1");
-    video.play();
-  }
-});
+// btn.addEventListener("click", function () {
+//   if (!btn.classList.contains("slide1")) {
+//     btn.classList.add("slide1");
+//     video.pause();
+//   } else {
+//     btn.classList.remove("slide1");
+//     video.play();
+//   }
+// });
 
 // preloader
-const preloader = document.querySelector(".preloader");
+// const preloader = document.querySelector(".preloader");
 
-window.addEventListener("load", function () {
-  preloader.classList.add("hide-preloader");
-});
+// window.addEventListener("load", function () {
+//   preloader.classList.add("hide-preloader");
+// });
 
 
 //MODAL
